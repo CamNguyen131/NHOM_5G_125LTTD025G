a9ba0e85670047962326c1c7b6743a1a0ac4a255
Uyen41 <lemyuyen2005@gmail.com>
2025-11-28 04:53:32 +0700

    commit 1

---
+++ app/src/main/java/com/example/uyen_ck/ListOrderActivity.java
@@ -0,0 +1,68 @@
+package com.example.uyen_ck;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_order);
+
+        // Xử lý click vào đơn hàng son kem lì MAC
+        LinearLayout cardMacMatte = findViewById(R.id.cardMacMatte); // Cần thêm id cho card này
+        cardMacMatte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOrderActivity.this, OrderDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        LinearLayout tabHome = findViewById(R.id.tabHome);
+        LinearLayout tabCart = findViewById(R.id.tabCart);
+        LinearLayout tabOrder = findViewById(R.id.tabOrder);
+        LinearLayout tabAccount = findViewById(R.id.tabAccount);
+
+        // Tab Tài khoản
+        tabAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOrderActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        // Tab Đơn hàng (đã ở trang này nên không cần làm gì)
+        tabOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Đã ở trang đơn hàng, không cần điều hướng
+            }
+        });
+
+        // Các tab khác
+        tabHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Điều hướng đến trang chủ
+            }
+        });
+
+        tabCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Điều hướng đến giỏ hàng
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/uyen_ck/SettingActivity.java
@@ -0,0 +1,26 @@
+package com.example.uyen_ck;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        // Xử lý nút back
+        ImageView backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Quay lại MainActivity
+            }
+        });
+    }
+}
